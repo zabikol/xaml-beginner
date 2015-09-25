7e31166f8154fa610ebe4af268cd28c6c3826be1
zabikol <zabikol-git@yahoo.com>
2015-09-25 20:07:17 +0100

    Module 3: Homework Submit 2
    Link pages by implementing buttons and adding home button on appbar.
    Note checked in with GIT not VS since VS doesn't add description?

--- RestaurantManager/RestaurantManager.UniversalWindows/ExpeditePage.xaml.cs
+++ RestaurantManager/RestaurantManager.UniversalWindows/ExpeditePage.xaml.cs
@@ -26,5 +26,10 @@ namespace RestaurantManager.UniversalWindows
         {
             this.InitializeComponent();
         }
+
+        private void Home_BarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
     }
 }
