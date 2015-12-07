a01480526a7073693276931e800709d94f05d45d
zabikol <zabikol-git@yahoo.com>
2015-12-07 14:08:34 +0000

    Converters and Dependency Property

--- RestaurantManager/RestaurantManager.UniversalWindows/MainPage.xaml.cs
+++ RestaurantManager/RestaurantManager.UniversalWindows/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -21,6 +22,7 @@ namespace RestaurantManager.UniversalWindows
         public MainPage()
         {
             this.InitializeComponent();
+            LoadData();
         }
 
         private void OrdersButton_Click(object sender, RoutedEventArgs e)
@@ -32,5 +34,70 @@ namespace RestaurantManager.UniversalWindows
         {
             Frame.Navigate(typeof(OrderPage));
         }
+
+        public bool? IsLoading
+        {
+            get { return (bool)GetValue(IsLoadingProperty); }
+            set { SetValue(IsLoadingProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsLoading.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsLoadingProperty =
+            DependencyProperty.Register("IsLoading", typeof(bool), typeof(MainPage), new PropertyMetadata(false));
+
+        private async void LoadData()
+        {
+            IsLoading = true;
+            await Task.Delay(5000);
+            IsLoading = false;
+        }
+    }
+
+    public class InvertBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool newValue = false;
+            if (value is bool)
+            {
+                newValue = (bool)value;
+            }
+            else if (value is bool?)
+            {
+                bool? tmp = (bool?)value;
+                newValue = tmp.HasValue ? tmp.Value : false;
+            }
+            return !(newValue);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool)
+            {
+                return !(bool)value;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public sealed class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+            => (value is bool && (bool)value) ? Visibility.Visible : Visibility.Collapsed;
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+            => value is Visibility && (Visibility)value == Visibility.Visible;
+    }
+
+    public class ValueConverterGroup : List<IValueConverter>, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string culture)
+            => this.Aggregate(value, (current, converter) => converter.Convert(current, targetType, parameter, culture));
+
+        public object ConvertBack(object value, Type targetType, object parameter, string culture)
+            => this.Reverse<IValueConverter>().Aggregate(value, (current, converter) => converter.ConvertBack(current, targetType, parameter, culture));
     }
 }
