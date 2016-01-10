9569e7aef47e129fcb4ebe36ea80d14ba71a1831
zabikol <zabikol-git@yahoo.com>
2016-01-10 18:57:30 +0000

    Implement View

---
+++ RestaurantManager/ViewApp/MessageDialogService.cs
@@ -0,0 +1,17 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewApp
+{
+    public sealed class MessageDialogService : IMessageService
+    {
+        public async void ShowDialog(string message)
+        {
+            await new Windows.UI.Popups.MessageDialog(message).ShowAsync();
+        }
+    }
+}
