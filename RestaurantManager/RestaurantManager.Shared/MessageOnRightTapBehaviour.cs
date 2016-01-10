c6b2ce14b748654da7e50697ec00660fb8b3d301
zabikol <zabikol-git@yahoo.com>
2016-01-10 16:45:12 +0000

    Extending XAML (IBehavior)
    Add Right Tap Behavior on the MainPage to show a thank you message
    (Module 3 - part 2)

---
+++ RestaurantManager/RestaurantManager.Shared/MessageOnRightTapBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+
+namespace RestaurantManager.Shared
+{
+    public class MessageOnRightTapBehaviour : DependencyObject, IBehavior
+    {
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register(nameof(Message), typeof(string), typeof(MessageOnRightTapBehaviour), new PropertyMetadata(string.Empty));
+
+        public DependencyObject AssociatedObject { get; private set; }
+
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            AssociatedObject = associatedObject;
+
+            if (AssociatedObject is UIElement)
+            {
+                (AssociatedObject as UIElement).RightTapped += PageRightTapped;
+            }
+        }
+
+        public void Detach()
+        {
+            if (AssociatedObject != null && AssociatedObject is UIElement)
+                (AssociatedObject as UIElement).RightTapped -= PageRightTapped;
+        }
+
+        private async void PageRightTapped(object sender, Windows.UI.Xaml.Input.RightTappedRoutedEventArgs e)
+        {
+            await new MessageDialog(Message).ShowAsync();
+        }
+    }
+}
