31fe786dc0f7ad9ee0bbcf28a77a830050e73f0e
zabikol <zabikol-git@yahoo.com>
2016-01-10 18:25:12 +0000

    Implement Model

--- RestaurantManager/Models/IMessageService.cs
+++ RestaurantManager/Models/IMessageService.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    public class Class1
+    public interface IMessageService
     {
+        void ShowDialog(string message);
     }
 }
---
+++ RestaurantManager/Models/Profile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public sealed class Profile
+    {
+        public string FullName { get; set; }
+        public DateTime HireDate { get; set; }
+        public int NumberOfCredentials { get; set; }
+    }
+}
